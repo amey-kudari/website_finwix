b554cc49f902429c84c8b8f0b86239f5237aa184
amey-kudari <work.ameykudari@gmail.com>
2020-01-18 23:01:12 +0530

    get data from python

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import json # javascript
 
 app = Flask(__name__)
 
+# company, parent, agency, city, primary offense, pen-year, pen-amount
+sdata = [
+    ['abc', 'Inc', 'federal', '', '308', '2018', '$100'],
+    ['abd', 'Inc', 'federal', '', '308', '2018', '$100'],
+    ['abe', 'Inc', 'federal', '', '308', '2018', '$100'],
+    ['abf', 'Inc', 'federal', '', '308', '2018', '$100'],
+    ['abg', 'Inc', 'federal', '', '308', '2018', '$100']
+]
+
 @app.route("/")
 def mainpage():
     return render_template("index.html")
 
+@app.route("/data")
+def gdata():
+    return json.dumps(sdata)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    db.create_all()
